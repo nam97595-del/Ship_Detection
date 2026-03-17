edd44eda0ff6aeb4402f8263cf8d8e9cdda35bae
namdo <nam97595@st.vimaru.edu.vn>
2026-03-17 19:48:00 +0700

    them DeepSORT

--- trackers/tracker_factory.py
+++ trackers/tracker_factory.py
@@ -1,8 +1,11 @@
 from .bytetrack_wrapper import ByteTrackWrapper
-
+from .deepsort_wrapper import DeepSortWrapper
 def create_tracker(tracker_type):
     if tracker_type == "ByteTrack":
         return ByteTrackWrapper()
+    elif tracker_type == "DeepOcSort":
+        return DeepSortWrapper()
     else:
         raise ValueError(f"Chưa hỗ trợ tracker: {tracker_type}")
+    
     
