4a32758af1c8401ee32cf4047a574a26e6254013
namdo <nam97595@st.vimaru.edu.vn>
2026-03-18 20:15:32 +0700

    them bot-sort

--- trackers/tracker_factory.py
+++ trackers/tracker_factory.py
@@ -1,10 +1,13 @@
 from .bytetrack_wrapper import ByteTrackWrapper
 from .deepsort_wrapper import DeepSortWrapper
+from .botsort_wrapper import BotSortWrapper
 def create_tracker(tracker_type):
     if tracker_type == "ByteTrack":
         return ByteTrackWrapper()
     elif tracker_type == "DeepOcSort":
         return DeepSortWrapper()
+    elif tracker_type == "BoT-SORT":
+        return BotSortWrapper()
     else:
         raise ValueError(f"Chưa hỗ trợ tracker: {tracker_type}")
     
