72f42c3c8c808acbe05a5abe20030b38be6f07a0
namdo <nam97595@st.vimaru.edu.vn>
2026-03-21 14:39:31 +0700

    them sfsort

--- trackers/tracker_factory.py
+++ trackers/tracker_factory.py
@@ -5,6 +5,7 @@ from .strongsort_wrapper import StrongSortWrapper
 from .boostrack_wrapper import BoostTrackWrapper
 from .hybridsort_wrapper import HybridSortWrapper
 from .ocsort_wrapper import OcSortWrapper
+from .sfsort_wrapper import SfSortWrapper
 def create_tracker(tracker_type):
     if tracker_type == "ByteTrack":
         return ByteTrackWrapper()
@@ -20,6 +21,8 @@ def create_tracker(tracker_type):
         return HybridSortWrapper()
     elif tracker_type == "OcSort":
         return OcSortWrapper()
+    elif tracker_type == "SfSort":
+        return SfSortWrapper()
     else:
         raise ValueError(f"Chưa hỗ trợ tracker: {tracker_type}")
     
