0d79516f043284082bf817170eeec5822dcc92f0
namdo <nam97595@st.vimaru.edu.vn>
2026-05-22 19:51:05 +0700

    update quantize

--- utils/export_engine.py
+++ utils/export_engine.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 from ultralytics import RTDETR
 
-#model = YOLO('E:\Lab Nghien Cuu\Ship_Boat\model_yolo\yolov12x_fish-speed-pass.pt')
-model = RTDETR('E:\Lab Nghien Cuu\Ship_Boat\model_rf\detrL-fish3(2).pt')
+model_path = r"E:\Download\yolo12x-fishv4\yolo12x-fish\weights\best.pt"
+model = YOLO(model_path)
+#model = RTDETR('E:\Download\yolo12x-fishv4\yolo12x-fish\weights\best.pt')
 print("Bắt đầu xuất mô hình sang TensorRT")
 
 
@@ -11,7 +12,7 @@ model.export(
     imgsz=640,          # Input Size
     half=True,          # Bật lượng tử hóa FP16
     dynamic=False,      # TẮT dynamic để tối ưu tốc độ tối đa
-    simplify=True,      # Rút gọn đồ thị nơ-ron
+    simplify=False,     # True = Rút gọn đồ thị nơ-ron
     workspace=6,        # Cho phép dùng 4GB VRAM để biên dịch
     batch=1             # Chạy từng frame một
 )
